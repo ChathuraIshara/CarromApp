9006f8a0e68f582749e88ec106fdd28ffa060f85
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-09-09 22:25:22 +0530

    create routes

---
+++ src/routes/routes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import SignIn from "../pages/Sign_in";
+import SignUp from "../pages/Sign_up";
+
+const RoutesComponent = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default RoutesComponent;
