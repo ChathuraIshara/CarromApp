ce0900aa7d3781f67d680fe59c7936bd5e78e9ec
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-09-13 23:36:20 +0530

    config

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -8,7 +8,7 @@ interface ButtonProps{
 }
 function SubmitButton({isloading,className,children}:ButtonProps) {
   return (
-   <Button type='submit'  disabled={isloading} className={className ?? 'w-full bg-purple-900 hover:bg-deep-purple-400 text-white h-11 rounded-md'}>
+   <Button type='submit'  disabled={isloading} className={className ?? 'w-full bg-primarypurple hover:bg-primarypurple text-white h-11 rounded-md'}>
     {isloading? (
         <div className='flex items center gap-4 '>
             <p>Loading...</p>
